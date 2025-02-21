59e12aa78c09436d34bae123c0de5ab67b827683
Facu <facubargut@gmail.com>
2025-02-21 01:20:31 +0100

    commit de prueba para ver si hace el cambio auto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatbotComponent } from './chatbot/chatbot';
 
@@ -8,6 +8,9 @@ import { ChatbotComponent } from './chatbot/chatbot';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    console.log('Esto es para ver si hace el build automatico...')
+  }
   title = 'chatbot';
 }
